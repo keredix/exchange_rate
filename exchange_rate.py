ea6cbc1e326876b5f2a0e44a6bd14380ab0e1c78
Keredix <marek.pniok@seznam.cz>
2025-08-10 18:31:38 +0200

    added date variable into the DB for better search

--- exchange_rate.py
+++ exchange_rate.py
@@ -1,9 +1,10 @@
 import requests
 import csv
 import json
-from dotenv import load_dotenv
 import os
 import sqlite3
+from datetime import datetime
+from dotenv import load_dotenv
 
 load_dotenv()
 api_key = os.getenv("API_KEY")
@@ -24,7 +25,7 @@ def get_data():
     if response.status_code == 200:
         data = json.loads(json.dumps(response.json()))
         exchange_rates = data.get("conversion_rates", {})
-        transformed_exchange_rates = [{'currency': k, 'rate': v} for k, v in exchange_rates.items()]
+        transformed_exchange_rates = [{'currency': k, 'rate': v, 'date': datetime.today().strftime('%Y-%m-%d')} for k, v in exchange_rates.items()]
         return transformed_exchange_rates
     # Return error code if the request fails
     else:
@@ -37,14 +38,15 @@ def load_data_to_db(exchange_rates):
     cursor = sqliteConnection.cursor()
 
     # Create table
-    cursor.execute('CREATE TABLE IF NOT EXISTS exchange_rates (id INTEGER PRIMARY KEY, currency TEXT UNIQUE, rate REAL)')
+    cursor.execute('CREATE TABLE IF NOT EXISTS exchange_rates (id INTEGER PRIMARY KEY, currency TEXT UNIQUE, rate REAL, date TEXT)')
 
     # Load the data to DB
     for row in exchange_rates:
-        cursor.execute('INSERT INTO exchange_rates (currency, rate) VALUES (?, ?) ON CONFLICT(currency) DO UPDATE SET rate = excluded.rate', (row['currency'], row['rate']))
+        cursor.execute('INSERT INTO exchange_rates (currency, rate, date) VALUES (?, ?, ?) ON CONFLICT(currency) DO UPDATE SET rate = excluded.rate', (row['currency'], row['rate'], row['date']))
 
     # Commit and close
     sqliteConnection.commit()
     sqliteConnection.close()
+
 
 main()
