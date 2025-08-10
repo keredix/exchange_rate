ab3dfaad676fed44b51074ad9cda13a48e875658
Keredix <marek.pniok@seznam.cz>
2025-08-10 15:52:35 +0200

    added load_data_to_db func

--- exchange_rate.py
+++ exchange_rate.py
@@ -3,13 +3,15 @@ import csv
 import json
 from dotenv import load_dotenv
 import os
+import sqlite3
 
 load_dotenv()
 api_key = os.getenv("API_KEY")
 
 
 def main():
-    get_data()
+    transformed_exchange_rates = get_data()
+    load_data_to_db(transformed_exchange_rates)
 
 
 # Function to get data from the API
@@ -21,9 +23,28 @@ def get_data():
     # Check status code of the request
     if response.status_code == 200:
         data = json.loads(json.dumps(response.json()))
-        exchange_rate = data.get("conversion_rates", {})
-        return exchange_rate
+        exchange_rates = data.get("conversion_rates", {})
+        transformed_exchange_rates = [{'currency': k, 'rate': v} for k, v in exchange_rates.items()]
+        return transformed_exchange_rates
+    # Return error code if the request fails
     else:
         print(f"Request failed with status code {response.status_code}")
+
+
+def load_data_to_db(exchange_rates):
+    # Connect to database
+    sqliteConnection = sqlite3.connect('rates.db')
+    cursor = sqliteConnection.cursor()
+
+    # Create table
+    cursor.execute('CREATE TABLE IF NOT EXISTS exchange_rates (id INTEGER PRIMARY KEY, currency TEXT UNIQUE, rate REAL)')
+
+    # Load the data to DB
+    for row in exchange_rates:
+        cursor.execute('INSERT INTO exchange_rates (currency, rate) VALUES (?, ?) ON CONFLICT(currency) DO UPDATE SET rate = excluded.rate', (row['currency'], row['rate']))
+
+    # Commit and close
+    sqliteConnection.commit()
+    sqliteConnection.close()
 
 main()
