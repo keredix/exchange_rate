1b38ffdf3a0fa1deee35de13151fd5f87dd13139
Keredix <marek.pniok@seznam.cz>
2025-08-14 15:22:03 +0200

    Refactors exchange rate retrieval and storage
    Replaces user input for currency selection with a direct USD base currency.
    
    Streamlines data retrieval from the API, transforms it into a
    consistent format and loads it into a SQLite database.
    
    Uses a single table for storing exchange rates, indexed by date and currency,
    to ensure uniqueness and allow for efficient updates.

--- exchange_rate.py
+++ exchange_rate.py
@@ -8,55 +8,48 @@ from dotenv import load_dotenv
 
 load_dotenv()
 api_key = os.getenv("API_KEY")
-
-
-def main():
-    currency_shortcut = get_currency()
-    transformed_exchange_rates = get_data(currency_shortcut)
-    load_data_to_db(transformed_exchange_rates,currency_shortcut)
-
-# Function to get currency from user
-def get_currency():
-    while True:
-        try:
-            currency_shortcut = str(input("Please enter currency: "))
-        except ValueError:
-            sys.exit("Please use supported shortcut for currency(example. USD, CZK, EUR)")
-        else:
-            return currency_shortcut
         
 
-# Function to get data from the API
-def get_data(currency_shortcut):
-    url = f"https://v6.exchangerate-api.com/v6/{api_key}/latest/{currency_shortcut}"
+# Function to get data from the API and transform them
+def get_data():
+    url = f"https://v6.exchangerate-api.com/v6/{api_key}/latest/USD"
     response = requests.get(url)
 
     # Check status code of the request
-    if response.status_code == 200:
-        data = json.loads(json.dumps(response.json()))
-        exchange_rates = data.get("conversion_rates", {})
-        transformed_exchange_rates = [{'date': datetime.today().strftime('%Y-%m-%d'), 'currency': k, 'rate': v} for k, v in exchange_rates.items()]
-        return transformed_exchange_rates
+    if response.status_code != 200:
+        raise Exception(f"Request failed with status code {response.status_code}")
     
-    elif response.status_code == 404:
-        sys.exit("Your request couldn't be completed, please try to use supported shortcut for currency(example. USD, CZK, EUR)")
-    
-    else:
-        sys.exit(f"Request failed with status code {response.status_code}")
+    data = json.loads(json.dumps(response.json()))
+    exchange_rates = data.get("conversion_rates", {})
+    transformed_exchange_rates = [{'date': "2025-08-13", 'currency': k, 'rate': v} for k, v in exchange_rates.items()]
+    return transformed_exchange_rates
+
+# Init database
+def init_db():
+    # Connect to DB
+    conn = sqlite3.connect('rates.db')
+    cursor = conn.cursor()
+
+    # Create table if not exist
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS rates (
+            id INTEGER PRIMARY KEY,
+            date TEXT,
+            currency TEXT,
+            rate REAL,
+            UNIQUE(date, currency)
+        )
+    """)
 
 # Func to load data to DB
-def load_data_to_db(exchange_rates,currency_shortcut):
-    # Connect to database
+def load_data_to_db(exchange_rates):
+    # Connect to DB
     sqliteConnection = sqlite3.connect('rates.db')
     cursor = sqliteConnection.cursor()
 
-    # Create table
-    sql_create = f'CREATE TABLE IF NOT EXISTS "{currency_shortcut}" (id INTEGER PRIMARY KEY, date TEXT, currency TEXT, rate REAL, UNIQUE(date,currency))'
-    cursor.execute(sql_create)
-
     # Load the data to DB
     for row in exchange_rates:
-        sql_insert = f'INSERT INTO "{currency_shortcut}" (date, currency, rate) VALUES (?, ?, ?) ON CONFLICT(date, currency) DO UPDATE SET rate = excluded.rate'
+        sql_insert = f'INSERT INTO rates (date, currency, rate) VALUES (?, ?, ?) ON CONFLICT(date, currency) DO UPDATE SET rate = excluded.rate'
         sql_data = (row['date'], row['currency'], row['rate'])
         cursor.execute(sql_insert,sql_data)
 
@@ -65,4 +58,7 @@ def load_data_to_db(exchange_rates,currency_shortcut):
     sqliteConnection.close()
 
 
-main()
+if __name__ == "__main__":
+    transformed_exchange_rates = get_data()
+    init_db()
+    load_data_to_db(transformed_exchange_rates)
